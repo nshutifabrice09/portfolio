4eaa677e26682b629175cb817dd65f83c2923e08
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2026-02-02 23:00:05 +0200

    chore: clean up empty code change sections in the changes log

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Portfolio } from './portfolio/portfolio';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:Portfolio},
+    { path: '**', redirectTo: '' }
+];
